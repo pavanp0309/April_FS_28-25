d8333f45f88719c3def082fb6189f175a16d178a
pavanp0309 <pavanreddyp0309@gmail.com>
2025-07-24 13:31:50 +0530

    array

---
+++ Javascript/arrays.js
@@ -0,0 +1,142 @@
+// why we use ?& where use
+// 1.to store grouph of data (it reduce storage_space)
+    //  to store grouph of cart items 
+    // mails,messages ,user data (login information of different)
+    // products
+    // esay for accessing and removing easy to perform
+    //  crud
+    // search filters 
+    // api_data
+
+// arrays are used for storing different kinds of data (data types (values)) under a single varibale name
+
+// different ways to create arrays 
+// 1.array literal method 
+// 2.using the string global keyword
+// 3.using the new keyword 
+
+// let a=[] // array literal method
+// console.log(typeof a)
+// // console.log(a.length) //property
+
+
+// let a1=[1,2,"hello",{name:"raju"},true]
+
+// // arrays always numbered indexed 
+// // accessing the data 
+// console.log(a1[0])
+// console.log(a1[1])
+
+// // using the loops 
+// // for(let i=0;i<a1.length;i++){
+// //     console.log("i",i)
+// //     console.log("val",a1[i])
+// // }
+
+// // for(let i in a1){
+// //     console.log(a1[i])
+// // }
+
+// // to modify the values
+//  a1[0]=10
+//  a1[3]="bye"
+//  console.log(a1)
+
+
+//  Methods 
+// Basic Methods 
+// push---> adding the element to the end of array 
+// pop--- used for removing the element from the last
+// shift---> used for removing an element from start
+// unshift ---> used for adding an element an element from start
+// slice:is used extracting the part of an arrays 
+// splice: which is used for adding and removing element from array 
+// indexof
+// lastindexof
+
+// higher order methods
+// map 
+// filter 
+// reduce 
+// foreach 
+// sort 
+// find  
+// findIndex 
+// reverse 
+
+
+// let bodyparts=["hair" ,"shirt" ,"pant" ,"shoes"]
+// Pop:Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+// console.log(bodyparts.pop())//shoes
+// console.log(bodyparts.pop())//shoes
+
+// Push :Appends new elements to the end of an array, and returns the new length of the array.
+// console.log(bodyparts)
+// console.log(bodyparts.push("shoepolish"))
+// console.log(bodyparts)
+
+// shift 
+// Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+// console.log(bodyparts.shift())//hair
+// console.log(bodyparts.shift())//shirt
+
+// unshift :
+// nserts new elements at the start of an array, and returns the new length of the array
+// console.log(bodyparts.unshift("oil"))
+// console.table(bodyparts)
+
+// let data=[1,2,3,4,5,6,7,8]
+// let even=[]
+// for(let i=0;i<data.length;i++){
+//     if(data[i]%2==0){
+//         even.push(data[i])
+//     }
+// }
+// console.log(even)
+
+
+// slice method slice(startindex,endindex)
+// console.log(bodyparts.slice()) // ['hair', 'shirt', 'pant', 'shoes']
+// console.log(bodyparts.slice(2)) // ['pant', 'shoes']
+// console.log(bodyparts.slice(1,3))//['shirt', 'pant']
+// console.log(bodyparts.slice(-1,-3)) // [] startindex>endindex==>empty
+
+// splice :splice(start: number, deleteCount?: number)
+// Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+// console.log(bodyparts.splice())//[]
+// console.log(bodyparts)
+// // // console.log(bodyparts.splice(1))//[]
+// // console.log(bodyparts.splice(1,1))//['shirt']
+// // console.log(bodyparts.splice(1,2))//['pant', 'shoes']
+//  console.log(bodyparts.splice(1,1,"raincourt","coverthebag"))
+//  console.log(bodyparts)
+
+
+// let idontknow=[1,2,3,4,5,6,7,8,9,10]
+// let r=idontknow.filter(ele=>ele%2==0)
+// console.log(r)
+
+
+// function whymethods(a){
+//  console.log(a)
+//  let newArray=[] // new array to add element that are multiplied
+//   for(let i=0;i<a.length;i++){
+//     console.log(a[i]*2)
+//     newArray.push(a[i]*2)
+//   }
+//   return newArray
+// }
+// let res=whymethods(idontknow)
+// console.log(res)
+
+// function newdata(e){
+// let neweven=[] // new array to add element that are multiplied
+//   for(let i=0;i<e.length;i++){
+//     if(e[i]%2==0){
+//      neweven.unshift(e[i])
+//     }
+//   }
+//   return neweven
+// }
+// let even=newdata(res)
+// console.log(even)
--- Javascript/loops.js
+++ Javascript/loops.js
@@ -138,19 +138,19 @@ function sumOfEvenNumber(n){
 // console.log(res)
 
 // write a function to reverse a number  
- function reverseNumber(str){
-  let n=str.toString()
-    console.log(n.length) //startpoint
-  let revesestr="" //to store the reversed string
-  for(let i=n.length-1;i>=0;i--){
-     console.log(n[i])
-     revesestr+=n[i]
-  }
- return revesestr
-}
+//  function reverseNumber(str){
+//   let n=str.toString()
+//     console.log(n.length) //startpoint
+//   let revesestr="" //to store the reversed string
+//   for(let i=n.length-1;i>=0;i--){
+//      console.log(n[i])
+//      revesestr+=n[i]
+//   }
+//  return revesestr
+// }
 
-let res1=reverseNumber(1234)
-console.log(res1)
+// let res1=reverseNumber(1234)
+// console.log(res1)
 
 // write a function to reverse a number  using for loop without using toString method  
 // write a function to count number vowels in a given string using the for loop 
@@ -160,28 +160,171 @@ console.log(res1)
 // write a function to print  sum even digits in a given number using the for loop 
 
 
-1.what are natural numbers  
-    def:
-     steps take an input user 5 
-     check for i natural or not 
-      repeat upto the n
-      print the number is natural or not 
+// 1.what are natural numbers  
+//     def:
+//  alogorithem steps   to write the code :
+//  usage 
+//      application 
 
-    usage 
-     application 
-  what are whole numbers
-2.what are composite numbers 
-3.what are integers 
-4.what are factors
-5.what are divisors and remainder and how to get those remainders and divisors
-6.what are prime numbers
-7.what is gcd 
-8.what is lcm 
-9 how to print the square and cubes of a numbers (steps for it)
-10 what is  amstrong number
-11 what is  strong number
-12 what is  perfect number
-13.how get last degits and how to remove last degits
-14.what is palindrome
-15 how to reverse a number 
+//   what are whole numbers
+// 2.what are composite numbers 
+// 3.what are integers 
+// 4.what are factors
+// 5.what are divisors and remainder and how to get those remainders and divisors
+// 6.what are prime numbers
+// 7.what is gcd /hcf
+// 8.what is lcm 
+// 9 how to print the square and cubes of a numbers (steps for it)
+// 10 what is  amstrong number
+// 11 what is  strong number
+// 12 what is  perfect number
+// 13.how get last degits and how to remove last degits
+// 14.what is palindrome
+// 15 how to reverse a number 
+
+
+// tell how to extract the digits form a number 
+// num=121--->how to extract one by digit  num%10--(remainder)
+// tell how to remove the digits form a number num/10--Quotient
+// while loop
+// natural number all positive number(countable number) upto number ,no negative nofraction no decimal
+
+// To reverse a number 
+// eg: 123---321
+
+// algorithm
+//  step1: take a number n
+//  step2: initilize a variable rev =0
+//  step3: write while num>0 perform 
+//        extract the last degit digit=n%10
+//        add it to rev varaible rev=rev*10+digit
+//        remove the last digit from a number n/10
+// step:print the rev 
+
+// function reverseNumber(num){//original num=123
+//   let rev=0;
+//   while(num>0){
+//     let digit=num%10
+//     console.log(digit)
+//     rev=rev*10+digit // 0*10+3=3
+//     console.log("rev",rev)
+//     num=Math.floor(num/10) // these modifies original number
+//     console.log("num",num)
+//   }
+//   return rev 
+
+// }
+// reverseNumber(123)// natural number
+// reverseNumber(0)// natural number
+// reverseNumber(-123)// natural number
+
+// 121--->121
+
+function palindrome(num){//original num=123
+  if(num<0) return false
+  if(num>0 && num<10) return true 
+  let original=num
+  let rev=0;
+  while(num>0){
+    let digit=num%10
+    console.log(digit)
+    rev=rev*10+digit // 0*10+3=3
+    console.log("rev",rev)
+    num=Math.floor(num/10) // these modifies original number
+    console.log("num",num)
+  }
+  return original===rev
+
+}
+
+// 121==121--palindrome 
+//-121==121--->not palindrome
+// 3
+// 0
  
+
+// Amstrong number /
+// 1234=4digits --power value
+  //  1^4+2^4+3^4+4^4==1234
+
+  //  alogorithem 
+  //  step1: take a number n
+  //  step2:count the Number of digitis using the while loop 
+  //  step3:calculate the power of each digit 
+  //  step4:total the power of each digit 
+  //  step5:check the total sum of digits is equal to original number
+
+  // write a code for counting number of digits from given number 
+
+  // algorithem
+  //  step1: take a number 
+  //  step2: initialize count varaible eg:count=0
+  //  step3:take while loop perform the action 
+  //        extract the last from given number : num%10
+  //        increment the count variable count ++
+  //       remove the last from given number : num=num/10
+  //   step4: repeat untill condition fails 
+  //   step5: print the count 
+
+// code: 
+function CountNumberDigits(num){
+  if(num===0) return 1 
+  num=Math.abs(num)// removes the negative values in numbers
+  let count=0
+  while(num>0){
+    // let digit=num%10 //extracts the last digits(not required)
+    count++
+    num=Math.floor(num/10) // removes the last digit from  a number
+  }
+  return count 
+} 
+
+console.log(CountNumberDigits(2))
+console.log(CountNumberDigits(223456))
+
+
+// write a code to calculate the sum of digits
+function sumofDigits(num){
+  if(num===0) return 1 
+  num=Math.abs(num)// removes the negative values in numbers
+  let sum=0
+  while(num>0){
+    let digit=num%10 //extracts the last digits(not required)
+    sum+=digit
+    num=Math.floor(num/10) // removes the last digit from  a number
+  }
+  return sum
+} 
+
+// write a code to calculate the product of digits
+
+function productofDigits(num){
+  if(num===0) return 1 
+  num=Math.abs(num)// removes the negative values in numbers
+  let product=1
+  while(num>0){
+    let digit=num%10 //extracts the last digits(not required)
+    product*=digit // product=product*digit
+    num=Math.floor(num/10) // removes the last digit from  a number
+  }
+  return product
+} 
+
+// Amstrong Number 
+
+function Amstrong(num){
+  let original=num
+  let digits=CountNumberDigits(num)
+  let sum=0
+
+  while(num>0){
+    let digit=num%10 
+    sum+=digit**digits
+    num=Math.floor(num/10)
+  }
+
+  return  original==sum
+
+}
+let data=Amstrong(123)
+console.log(data)
